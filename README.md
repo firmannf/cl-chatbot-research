eac2d644f8fb8b7e96b264dac78a9753d2bb2b13
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-12-21 17:58:56 +0700

    [UPDATE] Node.js Bot Sample - using API from Microsoft Bot Framework

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cl-chatbot-research
 Twikipedia - Codelabs road to Imagine Cup
+
+References:
+- Deploying your web app:
+https://docs.microsoft.com/en-us/azure/app-service-web/web-sites-deploy#git
+- Documentation, bot builder on Node.js:
+https://docs.botframework.com/en-us/node/builder/overview/#navtitle
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Add dependencies
-var restify = require(restify);
-var builder = require(botbuilder);
+var restify = require('restify');
+var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -11,8 +11,8 @@ server.listen(process.env.PORT || 3000, function() {
 // Create chat bot
 var connector = new builder.ChatConnector(
   {
-    appId: 'id',
-    appPassword: 'password'
+    appId: '0aa3c327-ba63-44f2-bd1b-371d0cde505b',
+    appPassword: 'saW3mpMraogCni38Y3MTJGF'
   }
 );
 var bot = new builder.UniversalBot(connector);
